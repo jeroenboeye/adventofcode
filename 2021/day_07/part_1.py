5a8e734cba86c0eab086f9faf01b442deca80ba6
jeroenboeye <jeroenboeye@gmail.com>
2021-12-07 20:24:22 +0100

    2021 day 7

---
+++ 2021/day_07/part_1.py
@@ -0,0 +1,81 @@
+"""Day 7 part 1 solution."""
+import argparse
+from pathlib import Path
+from typing import List
+
+import pytest
+
+INPUT_TXT = Path(__file__).parent / "input.txt"
+
+
+def sum_of_absolute_differences(numbers: List[int], value: int) -> int:
+    """Sum of absolute differences."""
+    return sum([abs(x - value) for x in numbers])
+
+
+def solve(text: str) -> int:
+    """Solve the puzzle."""
+    locations = [int(x) for x in text.split(",")]
+    midpoint = int(round(sum(locations) / len(locations)))
+    delta = sum_of_absolute_differences(locations, midpoint)
+
+    if delta > sum_of_absolute_differences(locations, midpoint + 1):
+        direction = 1
+        midpoint += 1
+    else:
+        direction = -1
+        midpoint -= 1
+
+    new_delta = sum_of_absolute_differences(locations, midpoint)
+    while new_delta < delta:
+        delta = new_delta
+        midpoint += direction
+        new_delta = sum_of_absolute_differences(locations, midpoint)
+
+    return delta
+
+
+INPUT_S = """\
+16,1,2,0,4,2,7,1,2,14
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 37),),
+)
+def test(input_s: str, expected: int) -> None:
+    """Check that the solution is correct."""
+    assert solve(input_s) == expected
+
+
+def main() -> int:
+    """Run the solution."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    parser.add_argument(
+        "-s",
+        "--submit",
+        action="store_const",
+        dest="submit_solution",
+        const=True,
+        default=False,
+    )
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        solution = solve(f.read())
+    print(solution)
+    if args.submit_solution:
+        from aocd import submit
+
+        print("Submitting solution.")
+        # Derive day from parent directory name, dirname should end in e.g. _01
+        day = int(Path(__file__).parent.absolute().name.split("_")[1])
+        submit(solution, year=2021, day=day)
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
