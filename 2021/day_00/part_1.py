badb3a503f70876a83f99caac58673d5f3fa635f
jeroenboeye <jeroenboeye@gmail.com>
2021-12-09 12:57:04 +0100

    also extract year from dirname for autosubmit

--- 2021/day_00/part_1.py
+++ 2021/day_00/part_1.py
@@ -46,9 +46,9 @@ def main() -> int:
         from aocd import submit
 
         print("Submitting solution.")
-        # Derive day from parent directory name, dirname should end in e.g. _01
-        day = int(Path(__file__).parent.absolute().name.split("_")[1])
-        submit(solution, year=2021, day=day)
+        # Derive year and day from parent directory name, dirname should end in e.g. /2021/day_01
+        full_path = Path(__file__).parent.absolute()
+        submit(solution, year=int(full_path.parent.name), day=int(full_path.name.split("_")[1]))
 
     return 0
 
