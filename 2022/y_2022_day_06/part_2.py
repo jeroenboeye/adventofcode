9347450db2500823f2f78da185a891ace087300f
jeroenboeye <jeroenboeye@gmail.com>
2022-12-07 21:59:48 +0100

    2022 day 6 solution

---
+++ 2022/y_2022_day_06/part_2.py
@@ -0,0 +1,66 @@
+"""Day 6 part 2 solution."""
+import argparse
+from collections import deque
+from pathlib import Path
+
+import pytest
+
+INPUT_TXT = Path(__file__).parent / "input.txt"
+
+
+def solve(text: str) -> int:
+    """Solve the puzzle."""
+    buffer: deque[str] = deque(maxlen=14)
+    for i, c in enumerate(text):
+        buffer.append(c)
+        if len(set(buffer)) == buffer.maxlen:
+            return i + 1
+    return -1
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    """Check that the solution is correct."""
+    assert solve(input_s) == expected
+
+
+def main() -> int:
+    """Run the solution."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    parser.add_argument(
+        "-s",
+        "--submit",
+        action="store_const",
+        dest="submit_solution",
+        const=True,
+        default=False,
+    )
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        solution = solve(f.read())
+    print(solution)
+
+    if args.submit_solution:
+        from aocd import submit
+
+        print("Submitting solution.")
+        # Derive year and day from parent directory name, dirname should end in e.g. /2021/y_2021_day_01
+        full_path = Path(__file__).parent.absolute()
+        submit(solution, year=int(full_path.parent.name), day=int(full_path.name.split("_")[-1]))
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
