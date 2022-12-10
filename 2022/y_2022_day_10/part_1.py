23d4c92c1eb6e416a95bcbe0a4edca90068d2026
jeroenboeye <jeroenboeye@gmail.com>
2022-12-10 20:24:11 +0100

    Day 10 solution

---
+++ 2022/y_2022_day_10/part_1.py
@@ -0,0 +1,223 @@
+"""Day 10 part 1 solution."""
+import argparse
+from pathlib import Path
+
+import pytest
+
+INPUT_TXT = Path(__file__).parent / "input.txt"
+
+
+def solve(text: str) -> int:
+    """Solve the puzzle."""
+    cycles_to_check = [20, 60, 100, 140, 180, 220]
+    signal_strength = 0
+    cycle = 0
+    x = 1
+    for line in text.splitlines():
+        if line == "noop":
+            cycle += 1
+            if cycle in cycles_to_check:
+                signal_strength += cycle * x
+        else:
+            _, value = line.split()
+            cycle += 1
+            if cycle in cycles_to_check:
+                signal_strength += cycle * x
+            cycle += 1
+            if cycle in cycles_to_check:
+                signal_strength += cycle * x
+            x += int(value)
+    return signal_strength
+
+
+INPUT_S = """\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+EXPECTED = 13140
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    """Check that the solution is correct."""
+    assert solve(input_s) == expected
+
+
+def main() -> int:
+    """Run the solution."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    parser.add_argument(
+        "-s",
+        "--submit",
+        action="store_const",
+        dest="submit_solution",
+        const=True,
+        default=False,
+    )
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        solution = solve(f.read())
+    print(solution)
+
+    if args.submit_solution:
+        from aocd import submit
+
+        print("Submitting solution.")
+        # Derive year and day from parent directory name, dirname should end in e.g. /2021/y_2021_day_01
+        full_path = Path(__file__).parent.absolute()
+        submit(solution, year=int(full_path.parent.name), day=int(full_path.name.split("_")[-1]))
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
---
+++ 2022/y_2022_day_10/part_2.py
@@ -0,0 +1,68 @@
+"""Day 10 part 2 solution."""
+import argparse
+import textwrap
+from pathlib import Path
+
+INPUT_TXT = Path(__file__).parent / "input.txt"
+
+
+def draw_pixel(x: int, cycle: int) -> str:
+    """Draw a pixel on the CRT."""
+    if abs(x - cycle % 40) < 2:
+        return "#"
+    else:
+        return "."
+
+
+def solve(text: str) -> str:
+    """Solve the puzzle."""
+    cycle = 0
+    x = 1
+    output = ""
+    for line in text.splitlines():
+        if line == "noop":
+            output += draw_pixel(x, cycle)
+            cycle += 1
+        else:
+            _, value = line.split()
+            output += draw_pixel(x, cycle)
+            cycle += 1
+            output += draw_pixel(x, cycle)
+            cycle += 1
+            x += int(value)
+
+    return "\n".join(textwrap.wrap(output, 40))
+
+
+def main() -> int:
+    """Run the solution."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    parser.add_argument(
+        "-s",
+        "--submit",
+        action="store_const",
+        dest="submit_solution",
+        const=True,
+        default=False,
+    )
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        solution = solve(f.read())
+    # solution = solve(INPUT_S)
+    print(solution)
+
+    if args.submit_solution:
+        from aocd import submit
+
+        print("Submitting solution.")
+        # Derive year and day from parent directory name, dirname should end in e.g. /2021/y_2021_day_01
+        full_path = Path(__file__).parent.absolute()
+        submit(solution, year=int(full_path.parent.name), day=int(full_path.name.split("_")[-1]))
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
